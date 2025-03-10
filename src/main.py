010ba5a27b6097173e89eb919f54133de1556028
Suzy Vaque <t-suzyvaque@microsoft.com>
2025-03-10 22:33:23 +0900

    Added content feedback

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 from run_stt import run_stt_flow
+from content_feedback import run_feedback_flow
 
 if __name__ == "__main__":
     pitch_purpose = "보안을 위한 코파일럿 사용"
-    run_stt_flow(pitch_purpose)
+    refined_text, refined_chunks = run_stt_flow(pitch_purpose)
+    feedback = run_feedback_flow(refined_chunks)
+    print("\n\n\nContent Feedback Based on MSLearn:", feedback)
