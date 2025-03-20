53fd4d322571b29a50f7064e9217dcbd5b58f1b6
minkyung01 <84274527+minkyung01@users.noreply.github.com>
2025-03-20 16:25:39 +0900

    extract pronunciation data

--- backend/src/airflow/dags/tasks/continuous_recognition.py
+++ backend/src/airflow/dags/tasks/continuous_recognition.py
@@ -2,8 +2,6 @@ import azure.cognitiveservices.speech as speechsdk
 from dotenv import load_dotenv
 import os
 import time
-import difflib
-import string
 import json
 
 def extract_data(): 
@@ -32,7 +30,7 @@ def extract_data():
     # set pronunciation config
     pronunciation_config = speechsdk.PronunciationAssessmentConfig( 
         grading_system=speechsdk.PronunciationAssessmentGradingSystem.HundredMark, 
-        granularity=speechsdk.PronunciationAssessmentGranularity.Phoneme, 
+        granularity=speechsdk.PronunciationAssessmentGranularity.Word, 
         enable_miscue=True
     ) 
     pronunciation_config.enable_prosody_assessment()
@@ -47,14 +45,10 @@ def extract_data():
 
     # initial setting
     done = False
-    reference_text = ""
-    recognized_words = []
+    word_accuracy_list = []
+    accuracy_scores = []
+    pron_scores = []
     fluency_scores = []
-    durations = []
-    speech_rates = []
-    confidence_scores = []
-    startOffset = 0
-    endOffset = 0
 
     ########## function ##########
     def stop_cb(evt: speechsdk.SessionEventArgs):
@@ -62,38 +56,32 @@ def extract_data():
         done = True
 
     def recognized(evt: speechsdk.SpeechRecognitionEventArgs):
-        nonlocal reference_text, recognized_words, fluency_scores, durations, startOffset, endOffset
+        nonlocal word_accuracy_list, accuracy_scores, pron_scores, fluency_scores
         pronunciation_result = speechsdk.PronunciationAssessmentResult(evt.result)
 
         # recognize
-        reference_text += evt.result.text + " "
-        recognized_words += pronunciation_result.words
-
-        fluency_scores.append(pronunciation_result.fluency_score)
-
         json_result = evt.result.properties.get(speechsdk.PropertyId.SpeechServiceResponse_JsonResult)
         jo = json.loads(json_result)
         nb = jo["NBest"][0]
-        
-        durations.extend([int(w["Duration"]) + 100000 for w in nb["Words"] if w["PronunciationAssessment"]["ErrorType"] == "None"])
-        if startOffset == 0:
-            startOffset = nb["Words"][0]["Offset"]
-        endOffset = nb["Words"][-1]["Offset"] + nb["Words"][-1]["Duration"] + 100000
+        words = nb['Words']
 
-        speech_rate = len(pronunciation_result.words) / int(endOffset - startOffset)
-        speech_rates.append(speech_rate)
+        recognized_words = [
+            {word_data['Word']: word_data['PronunciationAssessment']['AccuracyScore']}
+            for word_data in words
+        ]
 
-        confidence_score = nb["Words"][0]["Confidence"]
-        confidence_scores.append(confidence_score)
+        word_accuracy_list += recognized_words
+        accuracy_scores.append(pronunciation_result.accuracy_score)
+        pron_scores.append(pronunciation_result.pronunciation_score)
+        fluency_scores.append(pronunciation_result.fluency_score)
+
 
         # data during recognition
-        print("Accuracy score: {}, pronunciation score: {}, completeness score : {}, fluency score: {}, speech rate: {}, confidence score: {}".format(
-                pronunciation_result.accuracy_score, 
-                pronunciation_result.pronunciation_score,
-                pronunciation_result.completeness_score, 
-                pronunciation_result.fluency_score,
-                speech_rate * 10**7, # ms to s
-                confidence_score
+        print("Target Words: {}, Accuracy Score: {}, Pronunciation Score: {}, Fluency Score: {}".format(
+                recognized_words,
+                accuracy_scores,
+                pron_scores,
+                fluency_scores
         ))
 
     # connect events by recognizer
@@ -107,54 +95,14 @@ def extract_data():
         time.sleep(0.5)
     speech_recognizer.stop_continuous_recognition()
 
-    # convert reference_text to lower case, split to words, and remove punctuations
-    reference_words = [w.strip(string.punctuation) for w in reference_text.lower().split()]
-
-    # detect error words 
-    diff = difflib.SequenceMatcher(None, reference_words, [x.word.lower() for x in recognized_words])
-        
-    final_words = []
-    for tag, i1, i2, j1, j2 in diff.get_opcodes():
-        if tag in ['insert', 'replace']:
-            for word in recognized_words[j1:j2]:
-                word._error_type = 'Insertion'
-                final_words.append(word)
-                
-        if tag in ['delete', 'replace']:
-            for word_text in reference_words[i1:i2]:
-                word = speechsdk.PronunciationAssessmentWordResult({
-                    'Word': word_text,
-                    'PronunciationAssessment': {
-                        'ErrorType': 'Omission',
-                    }
-                })
-                final_words.append(word)
-            
-        if tag == 'equal':
-            final_words += recognized_words[j1:j2]
-
-    durations_sum = sum([d for w, d in zip(recognized_words, durations) if w.error_type == "None"])
-
-    # calculate whole accuracy by averaging
-    final_accuracy_scores = [word.accuracy_score for word in final_words if word.error_type != 'Insertion']
-    accuracy_score = sum(final_accuracy_scores) / len(final_accuracy_scores)
-
-    # re-calculate fluency score
-    fluency_score = (durations_sum / (endOffset - startOffset) * 100) if startOffset > 0 else 0
-
-    # calculate whole completeness score
-    handled_final_words = [w.word for w in final_words if w.error_type != "Insertion"]
-    completeness_score = len([w for w in final_words if w.error_type == "None"]) / len(handled_final_words) * 100
-    completeness_score = min(completeness_score, 100)
-
-    sorted_scores = sorted([accuracy_score, completeness_score, fluency_score])
-    pronunciation_score = sorted_scores[0] * 0.4 + sorted_scores[1] * 0.3 + sorted_scores[2] * 0.3
-
     return {
-        "pronunciation_score": round(pronunciation_score, 2),
-        "accuracy_score": round(accuracy_score, 2),
-        "completeness_score": round(completeness_score, 2),
-        "fluency_score": round(fluency_score, 2),
-        "speech_rate": round(sum(speech_rates) / len(speech_rates), 2),
-        "confidence_score": round(sum(confidence_scores) / len(confidence_scores), 2)
+        "avg_accuracy_score": round(sum(accuracy_scores) / len(accuracy_scores), 2),
+        "avg_pron_score": round(sum(pron_scores) / len(pron_scores), 2),
+        "avg_fluency_score": round(sum(fluency_scores) / len(fluency_scores), 2),
+        "bad_words": sorted(
+            word_accuracy_list, key=lambda x: list(x.values())[0]
+        )[:30]
     }
+
+result = extract_data()
+print('result: ', result)
